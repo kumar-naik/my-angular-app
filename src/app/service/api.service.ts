ea603a7f67870df738d244feec9347d4300ac518
kumar-naik <dnaik0806@gmail.com>
2020-05-24 22:10:32 -0400

    intial project

---
+++ src/app/service/api.service.ts
@@ -0,0 +1,42 @@
+import { LoginComponent } from './../login/login.component';
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import {User} from "../model/user.model";
+import {Observable} from "rxjs/index";
+import {ApiResponse} from "../model/api.response";
+
+@Injectable()
+export class ApiService {
+
+  constructor(private http: HttpClient) { }
+  baseUrl: string = 'http://localhost:8080/users/';
+
+  login(loginPayload) : Observable<ApiResponse> {
+    //return this.http.post<ApiResponse>;
+    alert("login@@@::http://localhost:9090/token/generate-token");
+    
+    return this.http.post<ApiResponse>('http://localhost:9090/' + 'token/generate-token', loginPayload);
+  }
+
+  getUsers() : Observable<ApiResponse> {
+    alert("getUsers");
+    return this.http.get<ApiResponse>('http://localhost:9090/zipkinTracer');
+  }
+
+  getUserById(id: number): Observable<ApiResponse> {
+    return this.http.get<ApiResponse>(this.baseUrl + id);
+  }
+
+  createUser(user: User): Observable<ApiResponse> {
+    alert("createUser**");
+    return this.http.post<ApiResponse>('http://localhost:9090/register', user);
+  }
+
+  updateUser(user: User): Observable<ApiResponse> {
+    return this.http.put<ApiResponse>(this.baseUrl + user.id, user);
+  }
+
+  deleteUser(id: number): Observable<ApiResponse> {
+    return this.http.delete<ApiResponse>(this.baseUrl + id);
+  }
+}
